e8af74b25e6a4fe5ea77ab6014583a4e20bd6f26
Ian K Smith <smithki707@gmail.com>
2019-03-16 17:52:14 -0600

    Add intial logic

--- src/AdaptableDiv.vue
+++ src/AdaptableDiv.vue
@@ -1,19 +1,81 @@
 <template>
-<div>Hello world</div>
+<div
+  class="overflow-hidden"
+  :style="{
+    'max-height': !!viewHeight ? `${viewHeight}px` : undefined,
+    'transition-property': 'max-height',
+    'transition-duration': `${transitionDuration}ms`,
+    'transition-timing-funtion': transitionTimingFunction,
+    'transition-delay': `${transitionDelay}ms`,
+  }">
+
+  <div ref="view">
+    <slot></slot>
+  </div>
+</div>
 </template>
 
 <script lang="ts">
 // --- Imports -------------------------------------------------------------- //
 
-// Vendor
+import { Subscription } from 'rxjs';
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
+import { watchResize } from 'watch-resize';
 
-// --- Component ------------------------------------------------------------ //
+// -------------------------------------------------------------------------- //
 
 @Component({
   name: 'AdaptableDiv',
 })
-export default class AdaptableDiv extends Vue {}
+export default class AdaptableDiv extends Vue {
+  // --- Data --- //
+
+  public viewEl!: HTMLDivElement | undefined;
+  public viewElResizeSubscription: Subscription | null = null;
+  public viewHeight: number = 0;
+  public viewWidth: number = 0;
+
+  // --- Props --- //
+
+  @Prop({ type: Number, default: 200 })
+  public transitionDuration!: number;
+
+  @Prop({ type: String, default: 'ease' })
+  public transitionTimingFunction!: string;
+
+  @Prop({ type: Number, default: 0 })
+  public transitionDelay!: number;
+
+  // --- Lifecycle --- //
+
+  public mounted() {
+    this.viewEl = this.$refs['view'] as HTMLDivElement;
+    this.setDimensions();
+    this.watchViewElForResize();
+  }
+
+  public beforeDestroy() {
+    if (this.viewElResizeSubscription) this.viewElResizeSubscription.unsubscribe();
+    this.viewElResizeSubscription = null;
+  }
+
+  // --- Business logic --- //
+
+  public async watchViewElForResize() {
+    await this.$nextTick();
+
+    const resize$ = await watchResize(this.viewEl!);
+    this.viewElResizeSubscription = resize$.subscribe(() => {
+      requestAnimationFrame(() => this.setDimensions());
+    });
+  }
+
+  public setDimensions() {
+    const boundingRect = this.viewEl!.getBoundingClientRect();
+    this.viewHeight = boundingRect.height;
+    this.viewWidth = boundingRect.width;
+  }
+}
 </script>
 
