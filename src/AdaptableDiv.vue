a30613cbd17734422b46d69db5dc13591d2fa467
Ian K Smith <smithki707@gmail.com>
2019-03-16 09:32:59 -0600

    Initial commit

---
+++ src/AdaptableDiv.vue
@@ -0,0 +1,20 @@
+<template>
+<div></div>
+</template>
+
+<script lang="ts">
+// --- Imports -------------------------------------------------------------- //
+
+// Vendor
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+
+// --- Component ------------------------------------------------------------ //
+
+@Component({
+  name: 'AdaptableDiv',
+  inheritAttrs: false,
+})
+export class AdaptableDiv extends Vue {}
+</script>
+
