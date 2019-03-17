158adf9a80eddf8f090e65eb2da09cc759a8ff3e
Ian K Smith <smithki707@gmail.com>
2019-03-17 10:28:24 -0600

    Remove "vue-property-decorator" & use plain object instead

--- src/AdaptableDiv.vue
+++ src/AdaptableDiv.vue
@@ -1,7 +1,7 @@
 <template>
 <div
   :style="{
-    'max-height': !!viewHeight ? `${viewHeight}px` : undefined,
+    'max-height': !!viewHeight ? `${viewHeight}px` : '100%',
     'transition-property': noTransition ? undefined : 'max-height',
     'transition-duration': noTransition ? undefined : `${transitionDuration}ms`,
     'transition-timing-funtion': noTransition ? undefined : transitionTimingFunction,
@@ -19,66 +19,69 @@
 // --- Imports -------------------------------------------------------------- //
 
 import { Subscription } from 'rxjs';
-import Vue from 'vue';
-import { Component, Prop } from 'vue-property-decorator';
+import Vue, { VueConstructor } from 'vue';
 import { watchResize } from 'watch-resize';
 
 // -------------------------------------------------------------------------- //
 
-@Component({
-  name: 'AdaptableDiv',
-})
-export default class AdaptableDiv extends Vue {
-  // --- Data --- //
-
-  public viewEl!: HTMLDivElement | undefined;
-  public viewElResizeSubscription: Subscription | null = null;
-  public viewHeight: number = 0;
-  public viewWidth: number = 0;
-
-  // --- Props --- //
-
-  @Prop({ type: Number, default: 200 })
-  public transitionDuration!: number;
-
-  @Prop({ type: String, default: 'ease' })
-  public transitionTimingFunction!: string;
-
-  @Prop({ type: Number, default: 0 })
-  public transitionDelay!: number;
-
-  @Prop(Boolean)
-  public noTransition!: boolean;
-
-  // --- Lifecycle --- //
-
-  public mounted() {
-    this.viewEl = this.$refs['view'] as HTMLDivElement;
-    this.setDimensions();
-    this.watchViewElForResize();
-  }
-
-  public beforeDestroy() {
-    if (this.viewElResizeSubscription) this.viewElResizeSubscription.unsubscribe();
-    this.viewElResizeSubscription = null;
-  }
-
-  // --- Business logic --- //
-
-  public async watchViewElForResize() {
-    await this.$nextTick();
-
-    const resize$ = await watchResize(this.viewEl!);
-    this.viewElResizeSubscription = resize$.subscribe(() => {
-      requestAnimationFrame(() => this.setDimensions());
-    });
-  }
-
-  public setDimensions() {
-    const boundingRect = this.viewEl!.getBoundingClientRect();
-    this.viewHeight = boundingRect.height;
-    this.viewWidth = boundingRect.width;
-  }
+export interface AdaptableDivData {
+  viewEl: HTMLDivElement | null;
+  viewElResizeSubscription: Subscription | null;
+  viewHeight: number;
+  viewWidth: number;
 }
+
+export default Vue.extend({
+  name: 'AdaptableDiv',
+
+  data(): AdaptableDivData {
+    return {
+      viewEl: null,
+      viewElResizeSubscription: null,
+      viewHeight: 0,
+      viewWidth: 0,
+    };
+  },
+
+  props: {
+    transitionDuration: { type: Number, default: 200 },
+    transitionTimingFunction: { type: String, default: 'ease' },
+    transitionDelay: { type: Number, default: 0 },
+    noTransition: Boolean,
+  },
+
+  methods: {
+    // --- Lifecycle --- //
+
+    mounted() {
+      this.viewEl = this.$refs['view'] as HTMLDivElement;
+      this.setDimensions();
+      this.watchViewElForResize();
+    },
+
+    beforeDestroy() {
+      if (this.viewElResizeSubscription) this.viewElResizeSubscription.unsubscribe();
+      this.viewElResizeSubscription = null;
+    },
+
+    // --- Business logic --- //
+
+    async watchViewElForResize() {
+      await this.$nextTick();
+
+      const resize$ = await watchResize(this.viewEl!);
+      this.viewElResizeSubscription = resize$.subscribe(() => {
+        requestAnimationFrame(() => this.setDimensions());
+      });
+    },
+
+    setDimensions() {
+      const boundingRect = this.viewEl!.getBoundingClientRect();
+      this.viewHeight = boundingRect.height;
+      this.viewWidth = boundingRect.width;
+    },
+  },
+});
 </script>
+
 
--- src/AdaptableDiv.vue.d.ts
+++ src/AdaptableDiv.vue.d.ts
@@ -1,16 +1,20 @@
 import { Subscription } from 'rxjs';
-import Vue from 'vue';
-export default class AdaptableDiv extends Vue {
-    viewEl: HTMLDivElement | undefined;
+import Vue, { VueConstructor } from 'vue';
+export interface AdaptableDivData {
+    viewEl: HTMLDivElement | null;
     viewElResizeSubscription: Subscription | null;
     viewHeight: number;
     viewWidth: number;
-    transitionDuration: number;
-    transitionTimingFunction: string;
-    transitionDelay: number;
-    noTransition: boolean;
+}
+declare const _default: VueConstructor<AdaptableDivData & {
     mounted(): void;
     beforeDestroy(): void;
     watchViewElForResize(): Promise<void>;
     setDimensions(): void;
-}
+} & {
+    transitionDuration: any;
+    transitionTimingFunction: any;
+    transitionDelay: any;
+    noTransition: any;
+} & Vue>;
+export default _default;
